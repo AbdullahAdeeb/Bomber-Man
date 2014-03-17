696b839f841f1bccc74ed4070e00813a3cf33b21
abdullahadeeb <abdullahadeeb@me4233-pc19.labs.sce.carleton.ca>
2014-03-17 12:55:47 -0400

    17/3/2014 fixed the null exception in the player added a periodic updater for all players

--- src/GameServer/Game.java
+++ src/GameServer/Game.java
@@ -10,6 +10,8 @@ import UDPCommunication.Connection;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  *
@@ -21,11 +23,22 @@ public class Game {
     MapView mapView;
     ArrayList<PlayerPawn> playerPawns;
     Connection com;
+    Timer timer;
 
     Game(String mapFilePath) {
+        timer = new Timer();
         this.mapModel = new MapModel(mapFilePath);
         playerPawns = new ArrayList();
         com = new Connection(new IncomingActionListener());
+
+        long updateX = 1000;
+        long updateY = 1000;
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                updateAllPlayers();
+            }
+        }, updateX, updateY);
 
     }
 
@@ -33,47 +46,52 @@ public class Game {
 
         @Override
         public void actionPerformed(ActionEvent e) {
+            synchronized (playerPawns) {
+                String actionCmd = e.getActionCommand();
+                int id = Character.getNumericValue(actionCmd.charAt(0));
+                int cmd = Character.getNumericValue(actionCmd.charAt(1)); //get the command
 
-            String actionCmd = e.getActionCommand();
-            int id = Character.getNumericValue(actionCmd.charAt(0));
-            int cmd = Character.getNumericValue(actionCmd.charAt(1)); //get the command
 
-
-            if (id < 0) { // add a new player .. when id is less than zero means id is not yet assigned
-                PlayerPawn p = new PlayerPawn(playerPawns.size(), mapModel);
-                playerPawns.add(p);
-                System.out.println("\n--RECEIVED CMD--\nNew Player: " + p.getId() + "\n");
-                com.sendID(String.valueOf(p.getId()));
-                id = p.getId();
-            } else if (cmd == 6) { // disconnect command
-                playerPawns.remove(id);
-                System.out.println("\n--RECEIVED CMD--\nPlayer Left: " + id + "\n");
-            } else if (cmd == 7) { // list current players
-                System.out.println(playerPawns.toArray());
-            } else if (cmd == 5) {
-                playerPawns.get(id).action(cmd);
-            } else {
-                if (id >= playerPawns.size()) {
-                    // TODO log it using logger
-                    System.out.println("ID Error: " + id);
-                    return;
+                if (id < 0) { // add a new player .. when id is less than zero means id is not yet assigned
+                    PlayerPawn p = new PlayerPawn(playerPawns.size(), mapModel);
+                    playerPawns.add(p);
+                    System.out.println("\n--RECEIVED CMD--\nNew Player: " + p.getId() + "\n");
+                    com.sendID(String.valueOf(p.getId()));
+                    id = p.getId();
+                } else if (cmd == 6) { // disconnect command
+                    playerPawns.remove(id);
+                    System.out.println("\n--RECEIVED CMD--\nPlayer Left: " + id + "\n");
+                } else if (cmd == 7) { // list current players
+                    System.out.println(playerPawns.toArray());
+                } else if (cmd == 5) {
+                    playerPawns.get(id).action(cmd);
+                } else {
+                    if (id >= playerPawns.size()) {
+                        // TODO log it using logger
+                        System.out.println("ID Error: " + id);
+                        return;
+                    }
+                    playerPawns.get(id).action(cmd);
+                    System.out.println("\n--RECEIVED CMD--\nPlayer: " + id + "\tCMD: " + cmd);
                 }
-                playerPawns.get(id).action(cmd);
-                System.out.println("\n--RECEIVED CMD--\nPlayer: " + id + "\tCMD: " + cmd);
+                com.send(id, 1, "8");  // send ACK
+                updateAllPlayers();
             }
-            com.send(id, 1, "8");  // send ACK
-            updateAllPlayers();
         }
+    }
 
-        public void updateAllPlayers() {
+    public void updateAllPlayers() {
+        synchronized (playerPawns) {
+            if (playerPawns.isEmpty()) {
+                return;
+            }
+
             System.out.println("updating all players >>>");
             String serialize = mapModel.serialize();
             for (int i = 0; i < playerPawns.size(); i++) {
                 com.send(playerPawns.get(i).getId(), 5, serialize);
             }
         }
-
-    
     }
 
     public static void main(String[] args) {
